858bb0f69e62e39ab410d7039beeb6bba945a848
ALİ KARA <77415061+aliikara@users.noreply.github.com>
2021-01-17 16:04:05 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi);
+            }
+        }
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.Adi} {musteri.Soyadi} Eklendi.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.Adi} {musteri.Soyadi} Silindi.");
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri()
+            {
+              Id = 1,
+              Adi = "Ali",
+              Soyadi = "KARA"
+            };
+            Musteri musteri2 = new Musteri()
+            {
+                Id = 2,
+                Adi = "Ahmet",
+                Soyadi = "KARA"
+            };
+            Musteri musteri3 = new Musteri()
+            {
+                Id = 3,
+                Adi = "Arif",
+                Soyadi = "KARA"
+            };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            musteriManager.Listele(musteriler);
+
+            musteriManager.Ekle(musteri1);
+
+            musteriManager.Sil(musteri3);
+
+            Console.ReadLine();
+
+        }
+    }
+}
